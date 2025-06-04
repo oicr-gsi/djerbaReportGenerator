ca5e3b1f9d0e9f4301dadb9ab169b34a9ca3b359
anallan <anallan@oicr.on.ca>
2025-06-04 14:00:41 -0400

    modified test

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd $1
 
-find . -name "*/*_report.research.html" | while read -r file; do
+find . -name "*_report.research.html" | while read -r file; do
   grep -v -e 'Requisition Approved:' -e 'Date of Report:' "$file" | md5sum
 done | sort -V
 
-find . -name "*/*_report.json" | while read -r file; do
+find . -name "*_report.json" | while read -r file; do
   # Remove known datetime fields using jq
   jq 'del(.core.extract_time, .plugins.case_overview.results.requisition_approved)' "$file" | md5sum
 done | sort -V
