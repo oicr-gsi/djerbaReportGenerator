bb4a3c91f5c78b55ec495dc2f2577afda7c17109
Aditi Nagaraj Nallan <anallan@oicr.on.ca>
2025-06-04 15:59:24 -0400

    Update calculate.sh

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-cd "$1" || exit 1
+cd $1
 
-find . -name "*_report.json" | while read -r file; do
-  # Remove known volatile fields
-  jq 'del(
-    .core.extract_time,
-    .plugins.case_overview.results.requisition_approved
-  )' "$file" | md5sum
+find . -name "*_report.research.html" | while read -r file; do
+  grep -v -e 'Requisition Approved:' -e 'Date of Report:' "$file" | md5sum
 done | sort -V
