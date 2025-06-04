82e6b63c20a3515e7207d50f5f3e68f35c85f293
Aditi Nagaraj Nallan <anallan@oicr.on.ca>
2025-06-04 15:57:15 -0400

    Update calculate.sh

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-cd $1
+cd "$1" || exit 1
 
 find . -name "*_report.json" | while read -r file; do
-  # Remove known datetime fields using jq
-  jq 'del(.core.extract_time, .plugins.case_overview.results.requisition_approved)' "$file" | md5sum
+  # Remove known volatile fields
+  jq 'del(
+    .core.extract_time,
+    .plugins.case_overview.results.requisition_approved
+  )' "$file" | md5sum
 done | sort -V
