7b2b177944361833689c7ec5edd06693891352a1
anallan <anallan@oicr.on.ca>
2026-09-01 15:54:48 -0400

    Updated the calculate.sh script

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 cd "$1"
-
-find . -name "*.tar.gz" | sort -V | while read -r tarball; do
-  while IFS= read -r html; do
-    tar -xOf "$tarball" "$html" \
-      | grep -v -e 'Requisition Approved:' -e 'Date of Report:' \
-      | md5sum \
-      | awk -v t="$tarball" -v h="$html" '{print $1, t ":" h}'
-  done < <(tar -tzf "$tarball" | grep -E '_report\.(research|clinical)\.html$' || true)
-done
-
+count=$(find . -name "*.tar.gz" | wc -l)
+echo "$1: $count"
